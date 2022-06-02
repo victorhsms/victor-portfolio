473c698a49297961b3225a8f28b1ca99423f303b
Victor Hugo <victorhsmsantos@gmail.com>
2022-06-02 16:58:12 -0300

    feat(style): estilização do menu de navegação

--- src/components/header/style.tsx
+++ src/components/header/style.tsx
@@ -4,16 +4,18 @@ export const Header = styled.header`
   display: flex;
   border-bottom: 1px solid #1e2d3d;
   font-size: 1rem;
+  color: #607b96;
 
   .title-container {
     width: 20%;
-    color: #607b96;
+    padding: 18px 0;
     border-right: 1px solid #1e2d3d;
     display: flex;
     align-items: center;
 
     strong {
       padding-left: 22px;
+      font-weight: 400;
     }
   }
 
@@ -21,5 +23,20 @@ export const Header = styled.header`
     width: 80%;
     display: flex;
     justify-content: space-between;
+
+    ul {
+      list-style-type: none;
+      display: flex;
+
+      li {
+        padding: 17px 31.5px;
+        border-right: 1px solid #1e2d3d;
+      }
+    }
+
+    span {
+      padding: 17px 31.5px;
+      border-left: 1px solid #1e2d3d;
+    }
   }
 `
